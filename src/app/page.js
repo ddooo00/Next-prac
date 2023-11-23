c5d7ab24427f00b25ccc72bff97b136a1adccd1a
ddooo00 <1995thdud@naver.com>
2023-11-23 22:05:58 +0900

    feat / 7.img file

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h2>Welcome</h2>
-    Hello, WEB!
+      <h2>Welcome</h2>
+      Hello, WEB!
+      <br></br>
+      <img src="/screenshot.png"></img>
     </>
-  )
+  );
 }
