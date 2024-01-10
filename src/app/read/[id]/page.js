d60c3262b8e92e4a43e3d10fb2f365f7bc6d2a6b
ddooo00 <1995thdud@naver.com>
2024-01-11 00:25:55 +0900

    feat / 15 . update(patch) / no cache

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,5 +1,8 @@
 export default async function Read(props) {
-  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`, {
+    cache: "no-store",
+    //글 목록이 수정되지 않는 이유는 캐시를 사용히서(cache HIT) = 캐쉬 끄기
+  });
   const topic = await resp.json();
   return (
     <>
